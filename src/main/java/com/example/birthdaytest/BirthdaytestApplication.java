61fcb8e7193a217d173828c2e18ec63ba0ebaf96
baiyujie <byj1575116@163.com>
2019-09-09 17:11:42 +0800

    first

---
+++ src/main/java/com/example/birthdaytest/BirthdaytestApplication.java
@@ -0,0 +1,13 @@
+package com.example.birthdaytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BirthdaytestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BirthdaytestApplication.class, args);
+    }
+
+}
